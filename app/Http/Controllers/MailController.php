613ce9e75e53d9c5f8a2634187f12a170bed891d
yuiko-ai <yuiko.aizawa@arsaga.jp>
2025-10-02 18:21:35 +0900

    メール受信一覧

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Mail;
+
+use Illuminate\Http\Request;
+
+class MailController extends Controller
+{
+    //mail一覧（受信一覧）
+    public function index()
+    {
+        $mails = Mail::where('recieve_user_id','=',auth()->id())->get();
+
+        return view('mail',compact('mails'));
+    }
+}
