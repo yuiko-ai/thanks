0b48b12957e6c085e369f3833291d08ca4c4d603
yuiko-ai <yuiko.aizawa@arsaga.jp>
2025-10-15 11:58:01 +0900

    アップデート項目にdepartmentを追加

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
+use App\Models\Department;
 
 class ProfileController extends Controller
 {
@@ -16,8 +17,10 @@ class ProfileController extends Controller
      */
     public function edit(Request $request): View
     {
+        $departments = Department::all(); //部署データ取得
         return view('profile.edit', [
             'user' => $request->user(),
+            'departments' => $departments, //bladeで$departmentsの変数が使える
         ]);
     }
 
@@ -31,6 +34,9 @@ class ProfileController extends Controller
         if ($request->user()->isDirty('email')) {
             $request->user()->email_verified_at = null;
         }
+
+
+        $request->user()->department = $request->department;//部署情報の更新
 
         $request->user()->save();
 
